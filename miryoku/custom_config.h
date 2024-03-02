6cb23e82a51d265b731a5746982ba3c14d4dc8ca
LJ <snapbeartrap@gmail.com>
2024-03-02 07:42:06 -0800

    switch = and - locations

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,8 +12,8 @@
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 &kp GRAVE  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp BACKSLASH \
-&kp MINUS  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
-&kp EQUAL  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp HASH \
+&kp EQUAL  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
+&kp MINUS  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp HASH \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
